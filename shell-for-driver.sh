7100a7a4591c5ed565e6d1499a9e582d6b7dbaef
aguamole <56515790+aguamole@users.noreply.github.com>
2019-10-15 01:20:42 -0300

    Add files via upload

--- shell-for-driver.sh
+++ shell-for-driver.sh
@@ -32,6 +32,27 @@ do
 		echo -e "\033[31mFalha arquivo não foi encontrado\033[m"; echo -e "Coloque o caminho do instalador oficial baixado do site da Nvidia\nCtrl+C para sair !"; read instalador
 	fi
 done
+#-------------------------------------#
+echo -e "\033[0;32m
+      _        _                         _     
+     | |      (_)                       | |    
+   __| | _ __  _ __   __ ___  _ __  ___ | |__  
+  / _  ||  __|| |\ \ / // _ \|  __|/ __||  _ \ 
+ | (_| || |   | | \ V /|  __/| | _ \__ \| | | |
+  \__ _||_|   |_|  \_/  \___||_|(_)|___/|_| |_|
+\033[0;34mVersion: 1\033[m
+"
+if lsmod | grep nvidia; then 	# Detecta se o driver Nvidia esta ativo
+	echo -e "\033[32mO driver da Nvidia foi detectado e já esta carregado.\033[m"
+	sleep 10; echo "Saindo..."; exit
+fi
+if lsmod | grep nouveau; then	# Detecta se o driver nouveau esta ativo e desativa.
+	echo -e "# Gerado pelo script driver.sh\nblacklist nouveau\noptions nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
+	update-initramfs -u
+	echo -e "O sistema sera reiniciado em 10 seg...\napós reinicio iniciar novamente o script.\nCtrl+C para cancelar !"
+	sleep 10
+	systemctl -i reboot
+fi
 echo -e " 
 \033[0;31mATENÇÃO\033[m\n---> Este script deve ser executado no terminal\npara acessar o terminal use as teclas Ctrl+Alt+F1.\nO computador precisa reiniciar e você tera\nque iniciar o script varias vezes ate terminar a instalação !\n
 "
@@ -50,25 +71,8 @@ if type /etc/init.d/lightdm; then echo "Detectado LightDM";
 	elif type /etc/init.d/sddm; then echo "Detectado SDDM";
 	/etc/init.d/sddm stop
 else
-	echo -e "\033[0;31mFalha não foi possível detectar o Direct Manager.\nRever linhas 40 a 55.\033[m"
+	echo -e "\033[0;31mFalha não foi possível detectar o Direct Manager.\nRever linhas 61 a 76.\033[m"
 	exit
-fi
-#-------------------------------------#
-echo -e "\033[0;32m
-      _        _                         _     
-     | |      (_)                       | |    
-   __| | _ __  _ __   __ ___  _ __  ___ | |__  
-  / _  ||  __|| |\ \ / // _ \|  __|/ __||  _ \ 
- | (_| || |   | | \ V /|  __/| | _ \__ \| | | |
-  \__ _||_|   |_|  \_/  \___||_|(_)|___/|_| |_|
-\033[0;34mVersion: 1\033[m
-"
-if lsmod | grep nouveau; then	# Detecta se o nouveau esta ativo e desativa.
-	echo -e "# Gerado pelo script driver.sh\nblacklist nouveau\noptions nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf
-	update-initramfs -u
-	echo -e "O sistema sera reiniciado em 10 seg...\napós reinicio iniciar novamente o script.\nCtrl+C para cancelar !"
-	sleep 10
-	systemctl -i reboot
 fi
 bash $instalador	# Inicia o instalador
 echo -e "O sistema sera reiniciado em 10 seg...\nCtrl+C para cancelar !"
